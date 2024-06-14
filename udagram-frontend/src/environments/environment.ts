3e10e92142e79d709e079ddcfe42d51bafa80ff3
Ubuntu <ubuntu@ip-172-31-54-184.ec2.internal>
2024-06-14 06:50:36 +0000

    update for kubernetes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0dae957b0fb74b1689674e777fdf116-1320231855.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
